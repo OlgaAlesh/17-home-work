cd6c4a0e8f9c58f2c31966bfcfb047fcc2675d0f
Aleshchenko Olga <mail_olga@inbox.ru>
2020-02-01 22:05:20 +0300

    task 4

--- js/script2.js
+++ js/script2.js
@@ -1,2 +1,26 @@
 // Задание 4
 
+let a = 1;
+
+if(a > 0){
+    console.log("Верно");
+} else{
+    console.log("Неверно");
+}
+
+a = 0;
+
+if(a > 0){
+    console.log("Верно");
+} else {
+    console.log("Неверно");
+}
+
+a = -3;
+
+if(a > 0){
+    console.log("Верно");
+} else{
+    console.log("Неверно");
+}
+
