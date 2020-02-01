490942bc63139ed8dce09ffd732f9b5305091104
Aleshchenko Olga <mail_olga@inbox.ru>
2020-02-01 22:14:18 +0300

    task 5

--- js/script3.js
+++ js/script3.js
@@ -1,2 +1,15 @@
 // Задание 5
 
+let b = 2;
+a = 10;
+let c = a + b;
+console.log(c);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+
+if(c > 1){
+    console.log(Math.pow(c,2));
+} else {
+    console.log(c);
+}
