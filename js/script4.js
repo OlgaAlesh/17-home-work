7bea8ccaaf59861d45c853e98050c8cbcd9c7430
Aleshchenko Olga <mail_olga@inbox.ru>
2020-02-01 22:47:06 +0300

    task 7

--- js/script4.js
+++ js/script4.js
@@ -1,2 +1,15 @@
-// Задание 6
+// Задание 7
+
+let n= prompt("Число от 0 до 59","");
+console.log(n);
+if(0<n && n<=15){
+    console.log("I четверть");
+} else if(15<n && n<=30){
+    console.log("II четверть");
+} else if(30<n && n<=45){
+    console.log("III четверть");
+} else{
+    console.log("IV четверть");
+}
+
 
