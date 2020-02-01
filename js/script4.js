8f43ec72005b787c7464ec3edf1f5c1057b392c8
Aleshchenko Olga <mail_olga@inbox.ru>
2020-02-01 22:56:24 +0300

    task 8

--- js/script4.js
+++ js/script4.js
@@ -2,7 +2,7 @@
 
 let n= prompt("Число от 0 до 59","");
 console.log(n);
-if(0<n && n<=15){
+if(n<=15){
     console.log("I четверть");
 } else if(15<n && n<=30){
     console.log("II четверть");
--- js/script5.js
+++ js/script5.js
@@ -1,2 +1,12 @@
-// Задание 7
+// Задание 8
+
+let day = prompt("Число от 1 до 31","");
+
+if(day<=10){
+    console.log("1 декада");
+} else if(day>10 && day <=20){
+    console.log("2 декада");
+}else{
+    console.log("3 декада");
+}
 
