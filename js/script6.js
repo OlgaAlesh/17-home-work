39fb4d1231e3ee50efdc5fc1fe3c145271c1cb59
Aleshchenko Olga <mail_olga@inbox.ru>
2020-02-01 23:23:48 +0300

    task 9

--- js/script6.js
+++ js/script6.js
@@ -1,2 +1,13 @@
-// Задание 8
+// Задание 9
 
+let numDay = prompt("Число дней","");
+
+numDay>=365 ? console.log(numDay/365 + " лет") : console.log("Меньше года");
+
+numDay>=31 ? console.log(numDay/31 + " месяцев") : console.log("Меньше месяца");
+
+numDay>=7 ? console.log(numDay/7 + " недель") : console.log("Меньше недели");
+
+console.log(numDay*24 + " час.");
+console.log(numDay*24*60 + " минут");
+console.log(numDay*24*60*60 + " секунд");
